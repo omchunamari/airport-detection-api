bd8289a3517b79bd09eba25520d59f97c8f23e67
omchunamari <chunamariom@gmail.com>
2025-03-12 22:25:35 +0530

    PPP

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def get_output_image(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
